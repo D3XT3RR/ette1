fe06881306f4479b3d3f2dcc87b8d681edda1689
Radek <rstepien@onet.eu>
2018-11-11 21:29:12 +0100

    Kontrola maksymalnego rozmiaru pliku

--- js/announAdd.js
+++ js/announAdd.js
@@ -2,7 +2,11 @@ $(document).ready( function() {
 $('.imgInp').bind('change', function() {
 
   //this.files[0].size gets the size of your file.
-  alert(this.files[0].size);
+  if(this.files[0].size > 2097152){
+  	alert("Plik jest za duży, wynosi " + Math.round(this.files[0].size/1024/1024 * 100)/100 + "MB a powinien być mniejszy niż 2MB");
+  	this.value = "";
+  }
+  
 
 });
 });
