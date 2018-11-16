64ea8a281cde4dad60d6af2acf8bac09573d238a
Radek <rstepien@onet.eu>
2018-11-16 20:18:53 +0100

    Edytowanie danych TERAZ na pewno działa xD

--- php/settings.php
+++ php/settings.php
@@ -1,5 +1,14 @@
 <?php
   require 'connect.php';
+  function random_str2($length, $keyspace = '0123456789abcde')
+{
+    $pieces = array();
+    $max = mb_strlen($keyspace, '8bit') - 1;
+    for ($i = 0; $i < $length; ++$i) {
+        $pieces []= $keyspace[rand(0, $max)];
+    }
+    return implode('', $pieces);
+}
   $session_user_id = $_SESSION['user']; 
   if(isset($_POST['oldpasswd']) && isset($_POST['newpasswd']) && isset($_POST['newpasswd2'])){
     $oldpasswd = $_POST['oldpasswd'];
@@ -13,7 +22,7 @@
       while($row = mysqli_fetch_assoc($raw_results))
       {
         if(substr($row['Password'],32) == SHA1($oldpasswd) && $newpasswd == $newpasswd2){
-          $newpasswd_updated = random_str(32).SHA1($newpasswd);
+          $newpasswd_updated = random_str2(32).SHA1($newpasswd);
           mysqli_query($link, "UPDATE login SET password = '$newpasswd_updated' WHERE ID = '$session_user_id'");
           echo("<script type='text/javascript'>alert('Hasło zostało zaktualizowane');</script>");
         }
