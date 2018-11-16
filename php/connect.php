9c013b088ba57d0501b40aafe854e8f125ba263a
Radek <rstepien@onet.eu>
2018-11-16 22:40:14 +0100

    Aktywacja/Dezaktywacja z master'a

--- php/connect.php
+++ php/connect.php
@@ -3,6 +3,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 $link = mysqli_connect("exten.nazwa.pl","exten_inkapital","Walter514","exten_inkapital");
+//$link = mysqli_connect("localhost", "root", "", "test");
 if (mysqli_connect_errno())
 {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
