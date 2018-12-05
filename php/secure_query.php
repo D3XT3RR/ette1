c1a8ba51ad129dc0626f0698b84e77ba817693a2
Weronika <32653324+Arrashi@users.noreply.github.com>
2018-12-05 13:55:51 +0100

    Obrazki są wyświetlane pokolei

--- php/secure_query.php
+++ php/secure_query.php
@@ -6,10 +6,11 @@ function secure_query($dblink, $query, $vartypes = array(), $params = array(), $
 	if(count($vartypes) > 0 && count($params) > 0){
 		call_user_func_array(array($stmt, "bind_param"), array_merge($vartypes, $dummy));
 	}
+	$keycount = 0;
 	foreach($image_data as $key => $img){
 		if($img != ""){
-			echo $key;
-			$stmt->send_long_data($key, $img);
+			$stmt->send_long_data($keycount, $img);
+			$keycount += 1;
 		}
 	}
 	
