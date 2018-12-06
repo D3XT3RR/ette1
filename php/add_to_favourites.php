832e988949f2069632a12030dc7cce256f6c695d
Weronika <32653324+Arrashi@users.noreply.github.com>
2018-12-06 13:04:14 +0100

    Ulubione cd.

--- php/add_to_favourites.php
+++ php/add_to_favourites.php
@@ -5,15 +5,26 @@
 	
 	$ad_id = @$_GET['id'];
 	$user_id = @$_SESSION['user'];
-	if(!empty($ad_id) && !empty($user_id) && is_numeric($ad_id)){
+	$action = @$_GET['action'];
+	if(!empty($ad_id) && !empty($user_id) && is_numeric($ad_id) && !empty($action)){
 		$result = secure_query($link,"SELECT Favourites FROM login WHERE ID=?",$t=array('i'),$a=array(&$user_id));
 		while($row = mysqli_fetch_row($result)){
 			$fav = $row[0];
 			$fav_arr = explode(",",$fav);
-			if(!in_array($ad_id, $fav_arr)){
-				array_push($fav_arr, $ad_id);
-				$fav_arr_imp = implode(",",$fav_arr);
-				secure_query($link,"UPDATE login SET Favourites = ? WHERE ID = ?",$t=array('si'),$a= array(&$fav_arr_imp, &$user_id));
+			if($action == 'add'){
+				if(!in_array($ad_id, $fav_arr)){
+					array_push($fav_arr, $ad_id);
+					$fav_arr_imp = implode(",",$fav_arr);
+					secure_query($link,"UPDATE login SET Favourites = ? WHERE ID = ?",$t=array('si'),$a= array(&$fav_arr_imp, &$user_id));
+				}
+			}
+			else if($action == 'remove'){
+				if(in_array($ad_id, $fav_arr)){
+					$index = array_search($ad_id, $fav_arr);
+					unset($fav_arr[$index]);
+					$fav_arr_imp = implode(",",$fav_arr);
+					secure_query($link,"UPDATE login SET Favourites = ? WHERE ID = ?",$t=array('si'),$a= array(&$fav_arr_imp, &$user_id));
+				}
 			}
 		}
 	}
