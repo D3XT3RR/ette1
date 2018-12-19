8c71d0a5014ee7f0d61f6b1702a8e31a18087459
Weronika <32653324+Arrashi@users.noreply.github.com>
2018-12-19 14:49:50 +0100

    AJAX - dodawanie do ulubionych

--- js/favourites.js
+++ js/favourites.js
@@ -1,7 +1,25 @@
 function AddToFavourites(id){
-	window.location = "php/add_to_favourites.php?action=add&id=" + id;
+	// old: window.location = "php/add_to_favourites.php?action=add&id=" + id;
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			document.getElementById("favBtn").src = 'style/image/ulubione.png';
+		}
+	};
+	xhttp.open("GET", "php/add_to_favourites.php?action=add&id=" + id, true);
+	xhttp.send();
+	
 }
 
 function RemoveFromFavourites(id){
-	window.location = "php/add_to_favourites.php?action=remove&id=" + id;
+	// old: window.location = "php/add_to_favourites.php?action=remove&id=" + id;
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			document.getElementById("favBtn").src = 'style/image/ulubione_nie.png';
+		}
+	};
+	xhttp.open("GET", "php/add_to_favourites.php?action=remove&id=" + id, true);
+	xhttp.send();
+	
 }
